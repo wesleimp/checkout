f3405ad1203f9008c8c916032d967ba4dbf55907
Weslei Juan Moser Pereira <wesleimsr@gmail.com>
2021-11-18 10:44:49 -0300

    Add cli handler

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.17
 require github.com/ktr0731/go-fuzzyfinder v0.5.1
 
 require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gdamore/encoding v1.0.0 // indirect
 	github.com/gdamore/tcell/v2 v2.4.0 // indirect
@@ -14,10 +15,13 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
-	github.com/stretchr/objx v0.3.0 // indirect
-	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/stretchr/testify v1.7.0
+	github.com/urfave/cli/v2 v2.3.0
 	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 )
+
+require github.com/urfave/cli v1.22.5 // indirect
--- main.go
+++ main.go
@@ -1,55 +1,77 @@
 package main
 
 import (
+	"errors"
 	"os"
 
 	"github.com/ktr0731/go-fuzzyfinder"
+	"github.com/urfave/cli/v2"
 
 	"github.com/wesleimp/checkout/internal/branch"
 	"github.com/wesleimp/checkout/internal/git"
 )
 
+var (
+	version = "v0.1.0"
+)
+
 func main() {
-	if !git.IsRepo() {
-		println("It's not a git repository")
-		os.Exit(1)
+	app := &cli.App{
+		Name:      "checkout",
+		Usage:     "Fuzzy-finding for checkouted branches in reflog",
+		UsageText: "checkout",
+		Version:   version,
+		Authors: []*cli.Author{{
+			Name:  "Weslei Juan Novaes Pereira",
+			Email: "wesleimsr@gmail.com",
+		}},
+		Action: func(c *cli.Context) error {
+			if !git.IsRepo() {
+				return errors.New("It's not a git repository")
+			}
+
+			current_branch, err := git.Run("rev-parse", "--abbrev-ref", "HEAD")
+			if err != nil {
+				return err
+			}
+
+			branches, err := branch.Checkouts(10000)
+			if err != nil {
+				return err
+			}
+
+			if !contains(branches, current_branch) {
+				branches = append(branches, current_branch)
+			}
+
+			idx, err := fuzzyfinder.Find(branches, func(i int) string {
+				return branches[i]
+			})
+			if err != nil {
+				if err.Error() != "abort" {
+					return err
+				}
+				return nil
+			}
+
+			branch := branches[idx]
+			out, err := git.Run("checkout", branch)
+			if err != nil {
+				return err
+			}
+
+			println(out)
+			println("switched to", branch)
+			return nil
+		},
 	}
 
-	current_branch, err := git.Run("rev-parse", "--abbrev-ref", "HEAD")
+	err := app.Run(os.Args)
 	if err != nil {
 		println(err.Error())
 		os.Exit(1)
 	}
 
-	branches, err := branch.Checkouts(10000)
-	if err != nil {
-		println(err.Error())
-		os.Exit(1)
-	}
-
-	if !contains(branches, current_branch) {
-		branches = append(branches, current_branch)
-	}
-
-	idx, err := fuzzyfinder.Find(branches, func(i int) string {
-		return branches[i]
-	})
-	if err != nil {
-		if err.Error() != "abort" {
-			println(err.Error())
-		}
-		os.Exit(0)
-	}
-
-	branch := branches[idx]
-	out, err := git.Run("checkout", branch)
-	if err != nil {
-		println(err.Error())
-		os.Exit(1)
-	}
-
-	println(out)
-	println("switched to", branch)
 	os.Exit(0)
 }
 
